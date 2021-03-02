e845a88f10bddb41647034beaa2161db82c1d1fc
Sajjad Ayobi <43270899+sajjjadayobi@users.noreply.github.com>
2021-03-02 14:33:41 +0330

    Update OnlineLrSetter.py

--- utils/OnlineLrSetter.py
+++ utils/OnlineLrSetter.py
@@ -1,19 +1,26 @@
 # alternative LROnPlateau 
 import pandas as pd
+import tensorflow as tf
 
-def create_lrs(inits_lrs=[5e-3, 5e-3], file_name='lrs'):
-  lrs = pd.DataFrame(data=inits_lrs, columns=['lrs'])
-  lrs.index.name = 'epoch'
-  lrs.to_csv(f'{file_name}.csv')
+class OnlineLr():
+  def __init__(self, init_lrs=[5e-3], path='lrs'):
+      lrs = pd.DataFrame(data=init_lrs, columns=['lrs'])
+      lrs.index.name = 'epoch'
+      self.path = path+'.csv'
+      lrs.to_csv(self.path)
+      del lrs
+  
+  def add_lrs(self, lr):
+      df = pd.read_csv(self.path)
+      df.append({'epoch': df.epoch.iloc[-1], 'lrs': lr}, ignore_index=True)
+      df.to_csv(f'{path}.csv')
+      del df
 
-# we need add_lrs in another kernel for add during training
-def add_lrs(path, lr):
-  df = pd.read_csv(f'{path}.csv')
-  df.append({'epoch': df.epoch.iloc[-1], 'lrs': lr}, ignore_index=True)
-  df.to_csv(f'{path}.csv')
+  def __call__(self, epoch):
+      df = pd.read_csv(self.path)
+      return df.lrs[epoch]
 
-def scheduler(epoch):
-  df = pd.read_csv('lrs.csv')
-  return df.lrs[epoch]
-
+scheduler = OnlineLr(init_lrs=[5e-5])
+scheduler(0) # 5e-5
+# using with fit
 callback = tf.keras.callbacks.LearningRateScheduler(schedule=scheduler, verbose=1)
